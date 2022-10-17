af9384d51f3b43d72d14698ff51dc91ad115a897
Marvis <marvis.dan@gmail.com>
2022-10-17 19:10:02 -0400

    add log to change workflows

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { mount } from "marketing/MarketingApp";
 import MarketingApp from "./components/MarketingApp";
-
+console.log("here");
 export default () => {
 	return (
 		<div>
