4e160354fd7635f230e7e476893dbde16c4e8205
Marvis <marvis.dan@gmail.com>
2022-10-20 08:51:11 -0400

    adding header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { mount } from "marketing/MarketingApp";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import { BrowserRouter as Router } from "react-router-dom";
 console.log("here");
 export default () => {
 	return (
-		<div>
-			<h1>Hi there!!!</h1>
-			<hr />
-			<MarketingApp />.
-		</div>
+		<Router>
+			<div>
+				<Header />
+				<MarketingApp />.
+			</div>
+		</Router>
 	);
 };
